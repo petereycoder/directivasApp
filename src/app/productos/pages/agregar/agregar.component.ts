5321a66858a0bb009a21c9fdd7041d7f7d7c6031
petereycoder <fp.pipe.reyes@gmail.com>
2022-03-14 19:12:47 -0600

    Directiva personaliza: nombre, mensaje y customIf

--- src/app/productos/pages/agregar/agregar.component.ts
+++ src/app/productos/pages/agregar/agregar.component.ts
@@ -9,6 +9,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class AgregarComponent {
 
+  texto1: string = 'Felipe Reyes';
+  color: string = 'red';
+
   miFormulario: FormGroup = this.fb.group({
     nombre: ['', Validators.required]
   });
@@ -17,6 +20,15 @@ export class AgregarComponent {
 
   tieneError( campo: string ): boolean{
     return this.miFormulario.get(campo)?.invalid || false;
+  }
+
+  cambiarNombre(){
+    this.texto1 = Math.random().toString();
+  }
+
+  cambiarColor(){
+    const color = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    this.color = color;
   }
 
 }
--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -1,18 +1,54 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[error-msg]'
 })
 export class ErrorMsgDirective implements OnInit{
 
+  private _color: string = 'red';
+  private _mensaje: string = 'Este campo es requerido';
+
   htmlElement: ElementRef<HTMLElement>;
+
+  @Input () set color( valor: string ){
+    this._color = valor;
+    this.setColor();
+  }
+
+  @Input () set mensaje( valor: string ){
+    this._mensaje = valor;
+    this.setMensaje();
+  }
+
+  @Input () set valido( valor: boolean ){
+    if(valor){
+      this.htmlElement.nativeElement.classList.add('hidden');
+    }else{
+      this.htmlElement.nativeElement.classList.remove('hidden');
+    }
+  }
 
   constructor( private el: ElementRef<HTMLElement>) { 
     this.htmlElement = el;
   }
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.setColor();
+    this.setMensaje();
+    this.setClase();
+  }
+
+  
+  setColor():void{
+    this.htmlElement.nativeElement.style.color = this._color;
+  }
+
+  setMensaje(): void{
+    this.htmlElement.nativeElement.innerText = this._mensaje;
+  }
+
+  setClase(): void{
+    this.htmlElement.nativeElement.classList.add('form-text');
   }
 
 }
